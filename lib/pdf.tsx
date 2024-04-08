1eb74f2d39a5d2756fb44a4ce831e2b4f7de51b4
TawanL <tawancraft@outlook.com>
2024-04-08 23:46:12 +0700

    Update flashcard creation logic and Gemini API integration

--- lib/pdf.tsx
+++
@@ -1,7 +0,0 @@
-import pdfToText from "react-pdftotext";
-
-export async function extractPDF(url: string) {
-  const pdfBlob = await fetch(url).then((res) => res.blob());
-  const text = await pdfToText(pdfBlob);
-  return text;
-}
