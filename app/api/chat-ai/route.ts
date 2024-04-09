0412c9f22ab291d405f55ecdf89a717faabd3ee1
TawanL <tawancraft@outlook.com>
2024-04-09 22:13:18 +0700

    Update chat-ai route to include document parameter in POST request

--- app/api/chat-ai/route.ts
+++ app/api/chat-ai/route.ts
@@ -3,13 +3,18 @@ import { getCompletion } from "@/lib/gemini";
 
 export async function POST(req: Request) {
   try {
-    const { message } = await req.json();
+    const { message, document } = await req.json();
 
     if (!message) {
       return new NextResponse("Bad Request", { status: 400 });
     }
 
-    const completion = await getCompletion(message);
+    const completion = document
+      ? await getCompletion(
+          `give a short answer to "${message}" that relate to "${document}"`
+        )
+      : await getCompletion(`give a short answer to "${message}"`);
+
     return new NextResponse(completion);
   } catch (error) {
     console.log("[CHAT AI]", error);
